79b6b0de4d7a35e4d0309618cce2cfa7310f8a34
ixiongdi <ixiongdi@gmail.com>
2013-05-13 16:01:21 +0800

    first 聪明咪咪兔

---
+++ Test.scala
@@ -0,0 +1,96 @@
+import com.google.caliper._
+import org.json4s._
+
+
+
+
+
+
+import scala.util.parsing.json._
+
+
+object Test {
+
+
+  val ss = scala.io.Source.fromFile("ss.json").mkString
+  
+  def main(args: Array[String]): Unit = {
+    println("-----------Start-----------------")
+    jackson
+    println("-------------------------------")
+    smart
+    println("-------------------------------")
+    native
+    println("-----------Stop-----------------")    
+  }
+
+
+  def smart {
+    import net.minidev.json.JSONValue
+    def test {
+      val v = JSONValue.parse(ss)
+      v.toString
+    }
+    val start = System.currentTimeMillis
+
+
+    MyBenchmark.timeMyOperation(1000, test)
+
+
+    val stop = System.currentTimeMillis
+
+
+    println("smart:   " + (stop - start) + "ms")
+  }
+
+
+  def native {
+    import org.json4s.native.JsonMethods._
+
+
+    def test {
+      val v = parse(ss)
+     render(v)
+    }
+    val start = System.currentTimeMillis
+
+
+    MyBenchmark.timeMyOperation(1000, test)
+
+
+    val stop = System.currentTimeMillis
+
+
+    println("native:   " + (stop - start) + "ms")
+  }
+
+
+  def jackson {
+    import org.json4s.jackson.JsonMethods._
+    def test {
+      val v = parse(ss)
+     render(v)
+    }
+    val start = System.currentTimeMillis
+
+
+    MyBenchmark.timeMyOperation(1000, test)
+
+
+    val stop = System.currentTimeMillis
+
+
+    println("jackson:  " + (stop - start) + "ms")
+  }
+
+
+}
+
+
+object MyBenchmark extends Benchmark {
+  def timeMyOperation(reps: Int, f: => Unit) = {
+    for (i <- 0 to reps) {
+      f
+    }
+  }
+}
---
+++ build.sbt
@@ -0,0 +1,6 @@
+libraryDependencies ++= Seq(
+  "com.google.caliper" % "caliper" % "1.0-beta-1",
+  "org.json4s" % "json4s-native" % "3.2.4",
+  "org.json4s" % "json4s-jackson" % "3.2.4",
+  "net.minidev" % "json-smart" % "2.0-RC2"
+)
